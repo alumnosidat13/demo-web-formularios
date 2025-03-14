fe04223168e035515da02c5d05b80dc8b4e7a2d2
alumnosidat13 <alumnosidat13@gmail.com>
2025-03-13 20:04:14 -0500

    vista y controller envio de objeto imc

--- src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
+++ src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
@@ -1,11 +1,31 @@
 package pe.edu.idat.demo_web_formularios.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.demo_web_formularios.model.ImcModel;
+
 @Controller
 public class ImcController {
     @GetMapping("/imc")
-    public String formularioImc(){
+    public String formularioImc(Model model){
+        model.addAttribute("imcmodel",
+                new ImcModel());
         return "imc";
     }
+
+    @PostMapping("/calcularimc")
+    public String calcularImc(@ModelAttribute("imcmodel")
+                                  ImcModel imc,
+                              Model model){
+
+        return "imc";
+    }
+
+
+
+
+
 }
