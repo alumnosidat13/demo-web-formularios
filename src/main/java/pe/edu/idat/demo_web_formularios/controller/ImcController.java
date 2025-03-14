c86f0d3e0ba81cc71154e49674103a5e3c3b6cb7
alumnosidat13 <alumnosidat13@gmail.com>
2025-03-13 20:28:47 -0500

    enviado datos del controller a la vista

--- src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
+++ src/main/java/pe/edu/idat/demo_web_formularios/controller/ImcController.java
@@ -20,7 +20,25 @@ public class ImcController {
     public String calcularImc(@ModelAttribute("imcmodel")
                                   ImcModel imc,
                               Model model){
-
+        Double tallam = imc.getTalla() / 100;
+        Double valorImc = imc.getPeso() / (tallam * tallam);
+        String diagnostico = "";
+        if(valorImc <= 18.5){
+            diagnostico = "Por debajo del peso.";
+        } else if(valorImc <= 25){
+            diagnostico = "con peso normal";
+        } else if(valorImc <= 30){
+            diagnostico = "con sobrepeso";
+        } else if(valorImc <= 36){
+            diagnostico = "obesidad leve";
+        } else if(valorImc <= 39){
+            diagnostico = "obsidad media";
+        }else {
+            diagnostico = "obesidad mórbida";
+        }
+        model.addAttribute("resultado",
+                "Su valor IMC = " + valorImc +
+                ", usted se encuentra: " +diagnostico);
         return "imc";
     }
 
